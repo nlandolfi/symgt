98369f84db911c58330e92658762b77f2cda5011
Nicholas C. Landolfi <nicholas.charles@landolfi.org>
2023-05-18 14:36:56 -0700

    experiments

--- experiments/main.py
+++ experiments/main.py
@@ -9,7 +9,7 @@ def matrix(df):
     )
 
 
-batches = pd.read_csv("./64_batches_first_two_months.csv")
+batches = pd.read_csv("./batches.csv")
 X = matrix(batches)
 
 m_sym = st.ExchangeableModel.fit(X)
@@ -17,8 +17,37 @@ m_iid = st.IIDModel.fit(X)
 q_sym = np.exp(m_sym.log_q())
 q_iid = np.exp(m_iid.log_q())
 
-mu_sym, cost_sym = st.optimal_multiplicity_function(q_sym)
-mu_iid, cost_iid = st.optimal_multiplicity_function(q_iid)
-l_sym, l_iid = st.integer_partition(mu_sym), st.integer_partition(mu_iid)
-print(st.integer_partition(mu_sym), cost_sym)
-print(st.integer_partition(mu_iid), cost_iid)
+mu_sym, _ = st.optimal_multiplicity_function(q_sym)
+mu_iid, _ = st.optimal_multiplicity_function(q_iid)
+mu_drf = st.dorfman_multiplicity_function(m_iid.n, m_iid.prevalence())
+l_sym, l_iid, l_drf = st.integer_partition(mu_sym), st.integer_partition(mu_iid), st.integer_partition(mu_drf)
+
+def sizes(a):  # for compatability with the old julia code
+    b = st.integer_partition(a)
+    b.reverse()
+    return b
+
+
+def array(multfn):
+    num_groups = np.sum(multfn)
+    A = np.zeros(
+        (num_groups, len(multfn) - 1),
+    )  # - 1 since multfn starts at 0
+    j = 0
+    for i, s in enumerate(sizes(multfn)):
+        for k in range(j, j + s):
+            A[i, k] = 1
+            j += 1
+    return A.T  # switch to rows are samples
+
+
+def tests_expended(multfn, samples):
+    # samples is N by n
+    A = array(multfn)  # n by g
+    R = samples @ A  # N by g
+    return np.sum(R @ sizes(multfn)) + A.shape[1] * samples.shape[0]
+
+print(m_sym.alpha)
+print(l_drf, st.ECost(mu_drf, q_sym), tests_expended(mu_drf, X))
+print(l_iid, st.ECost(mu_iid, q_sym), tests_expended(mu_iid, X))
+print(l_sym, st.ECost(mu_sym, q_sym), tests_expended(mu_sym, X))
