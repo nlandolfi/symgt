298ea8d82575cae3b43b4cf116c7b15c5b5f5c0c
Nicholas C. Landolfi <nicholas.charles@landolfi.org>
2023-05-18 22:16:41 -0700

    more cleanup

--- src/symgt/__init__.py
+++ src/symgt/__init__.py
@@ -1,14 +1,10 @@
 from .models import IIDModel, ExchangeableModel
 from .algorithms import (
-    dorfman_pool_size,
-    dorfman_multfn,
     optimal_multfn,
 )
 
 __all__ = [
     "IIDModel",
     "ExchangeableModel",
-    "dorfman_pool_size",
-    "dorfman_multfn",
     "optimal_multfn",
 ]
--- tests/03_smoke_test:_algorithms.py
+++ tests/03_smoke_test:_algorithms.py
@@ -3,7 +3,6 @@ import pytest
 
 from symgt.algorithms import dorfman_pool_size
 from symgt.algorithms import dorfman_multfn
-import symgt as st
 
 # test other things, ECost etc.
 print("THIS IS SMOKE TEST 3: IT TESTS algorithms.py")
@@ -20,15 +19,3 @@ with pytest.raises(ValueError):
     dorfman_pool_size(0.1, max_pool_size=-1)
 
 assert np.all(dorfman_multfn(5, 0.15) == [0, 0, 1, 1, 0, 0])
-
-
-def test_dorfman_etc():
-    assert st.dorfman_pool_size(0.01) == 11
-    assert st.dorfman_pool_size(0.02) == 8
-    assert st.dorfman_pool_size(0.05) == 5
-    assert st.dorfman_pool_size(0.1) == 4
-    assert st.dorfman_pool_size(0.15) == 3
-    assert np.all(st.dorfman_multfn(5, 0.15) == [0, 0, 1, 1, 0, 0])
-
-
-test_dorfman_etc()
