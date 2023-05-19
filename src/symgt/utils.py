28cb973d29f06e381076789f7664cd13563fa796
Nicholas C. Landolfi <nicholas.charles@landolfi.org>
2023-05-18 22:00:36 -0700

    comment cleanup

--- src/symgt/utils.py
+++ src/symgt/utils.py
@@ -6,7 +6,10 @@ def integer_partition_from_multfn(multfn: np.ndarray) -> np.ndarray:
     Convert a multiplicity function to an integer partition.
     An integer partition is a nondescreasing list of (possibly repeating) part sizes.
 
-    `multfn[i]` is the multiplicty of a part of size i
+    Parameters
+    ----------
+    multfn : np.ndarray
+        `multfn[i]` is the multiplicty of a part of size i
     """
     ss = []
     for i, x in enumerate(multfn):
@@ -20,11 +23,15 @@ def U_from_q(q: np.ndarray) -> np.ndarray:
     """
     Compute the function `U` for the symmetric distribution represented by `q`.
 
-    U : {0,…,n} → R
+    U : {0, …, n} → R
     U(h) is the expected number of tests used to declare a group of size `h`.
 
-
     This is a helper function for `optimal_multfn` below.
+
+    Parameters
+    ----------
+    q : np.ndarray
+        `q[i]` is the probability that a group of size i tests negative.
     """
     # assert is_plausible_q(q)
     n = len(q) - 1
@@ -45,9 +52,12 @@ def ECost(q: np.ndarray, multfn: np.ndarray) -> float:
     Compute the expected cost of a grouping encoded by `multfn` for a
     symmetric distribution with representation `q`.
 
-    `multfn[i]` is the number of parts of size `i`.
-    `q[i]` is the probability that a group of size i tests negative.
-
+    Parameters
+    ----------
+    multfn : np.ndarray
+        `multfn[i]` is the number of parts of size `i`.
+    q : np.ndarray
+        `q[i]` is the probability that a group of size i tests negative.
     """
     return np.sum([m * ETests(q, h) for (h, m) in enumerate(multfn)])
 
@@ -57,8 +67,12 @@ def ETests(q: np.ndarray, h: int) -> float:
     Compute the expected number of tests used for a group of size `h`
     for a symmetric distribution with representation `q`.
 
-    `h` is the size of the group.
-    `q[i]` is the probability that a group of size i tests negative.
+    Parameters
+    ----------
+    q : np.ndarray
+        `q[i]` is the probability that a group of size i tests negative.
+    h : int
+        `h` is the size of the group.
     """
     if h == 1:
         return 1
