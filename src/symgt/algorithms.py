582278d210bec2e1706087e19ded3aaf0b96a7ff
Nick Landolfi <nicholas.charles@landolfi.org>
2023-05-19 11:48:31 -0700

    multfn computation for additive intpart problems (#10)
    * multfn computation for additive intpart problems
    
    * typo
    
    * comments
    
    * comment improvements
    
    * comments
    
    * checkpoint
    
    * cleanup algorithm code
    
    * checkpoint
    
    * checkpoint
    
    * use language istar
    
    * more cleanup of comments
    
    * more cleanup
    
    * checkpoint
    
    * checkpoint
    
    * checkpoint
    
    * checkpoint

--- src/symgt/algorithms.py
+++ src/symgt/algorithms.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from .utils import U_from_q
+
 
 def dorfman_pool_size(prevalence: float, max_pool_size: int = 100) -> int:
     """
@@ -44,3 +46,100 @@ def dorfman_multfn(n: int, prevalence: float) -> np.ndarray:
     if n % m != 0:
         multfn[n % m] = 1  # remainder
     return multfn
+
+
+def compute_optimal_multfn(c: np.ndarray, subproblems=False):
+    """
+    Compute an optimal multiplicity function for cost `c` where `c[i]` is the
+    cost of a part of size `i`. The size of the largest part `n` is inferred
+    from c (i.e., `n = len(c) - 1`). The value `c[0]` is not used.
+
+    We use dynamic programming. We do not compute all optimal multiplicity
+    functions, just a single one.
+
+    Use the keyword argument `subproblems=true` to return multiplicity
+    functions and costs for all subproblems. The multiplicity functions
+    are the rows of the first value returned. The second value is the costs.
+
+    Examples
+    --------
+    To just get the solution for `n = len(c) - 1`:
+    ```
+        multfn, cost = compute_optimal_multfn(c)
+    ```
+    Here `multfn` is an nd.array and `cost` is a float.
+
+    To get solutions and costs for all subproblems:
+    ```
+        multfns, costs = compute_optimal_multfn(c, subpopulations=true)
+    ```
+    Here `multfns[i, :]` is an optimal multiplicty function for a subpopulation
+    of size `i` and `costs[i]` is its cost.
+    """
+    # c[i] is the cost of a part of size i = 0, …, n
+    n = len(c) - 1
+
+    if not (n > 0):
+        raise ValueError(f"population size n={n} should be > 0")
+
+    # Mstar[m] is the optimal cost to partition m = 0, …, n (the value function)
+    Mstar = np.zeros(n + 1)  # note, Mstar[0] = 0 by default
+
+    # The cost of partitioning 1 is c[1]
+    Mstar[1] = c[1]
+
+    # the n+1 here is for indexing convenience, we don't use istar[0] or multfns[0, :]
+    istar = np.zeros(n + 1, dtype=int)
+    multfns = np.zeros((n + 1, n + 1), dtype=int)
+
+    for k in range(1, n + 1):
+        # find an optimal istar[k]
+        istar[k] = np.argmin([Mstar[k - i] + c[i] for i in range(1, k + 1)]) + 1
+
+        # record the optimal cost
+        Mstar[k] = Mstar[k - istar[k]] + c[istar[k]]
+
+        if k - istar[k] > 0:  # if we are using a subproblem
+            multfns[k, :] = multfns[k - istar[k], :]  # take its multfn
+        # otherwise, inherit the zero pattern
+
+        # update the multfn to include a part of size istar[k]
+        multfns[k, istar[k]] += 1
+
+    # row i should be a be a multfn for i
+    w = np.arange(0, n + 1)
+    got, want = multfns @ w, w
+    assert np.all(got == want), f"multfns @ np.arange(1, n+1): got {got} want {want}"
+
+    if subproblems:
+        return multfns, Mstar
+    else:
+        return multfns[n, :], Mstar[n]
+
+
+def symmetric_multfn(q: np.ndarray, subproblems=False):
+    """
+    Compute an optimal multiplicity function for a symmetric distribution
+    with representation `q`. The population size `n` is inferred from the
+    length of `q` (i.e., `len(q) - 1`).
+
+    The keyword argument `subproblems=true` behaves as in `compute_optimal_multfn`.
+
+    Examples
+    --------
+    e.g.,
+    ```
+        multfns, costs = optimal_multfn(q, subpopulations=true)
+    ```
+    Here `multfns[i, :]` is an optimal multiplicty function for a subpopulation
+    of size `i` and `costs[i]` is its cost.
+    """
+    n = len(q) - 1
+
+    if not (n > 0):
+        raise ValueError(f"population size n={n} should be > 0")
+
+    # U[h] is the *expected* number of tests used for a group of size h = 0, …, n
+    U = U_from_q(q)
+
+    return compute_optimal_multfn(U, subproblems=subproblems)
--- tests/03_smoke_test:_algorithms.py
+++ tests/03_smoke_test:_algorithms.py
@@ -1,11 +1,16 @@
 import numpy as np
 import pytest
 
-from symgt.algorithms import dorfman_pool_size
-from symgt.algorithms import dorfman_multfn
+from symgt.algorithms import (
+    dorfman_pool_size,
+    dorfman_multfn,
+    compute_optimal_multfn,
+    symmetric_multfn,
+)
 
 print("THIS IS SMOKE TEST 3: IT TESTS algorithms.py")
 
+# Test dorfman_pool_size
 assert dorfman_pool_size(0.01) == 11
 assert dorfman_pool_size(0.02) == 8
 assert dorfman_pool_size(0.05) == 5
@@ -17,4 +22,22 @@ with pytest.raises(ValueError):
 with pytest.raises(ValueError):
     dorfman_pool_size(0.1, max_pool_size=-1)
 
+# Test dorfman_multfn
 assert np.all(dorfman_multfn(5, 0.15) == [0, 0, 1, 1, 0, 0])
+
+# Test compute_optimal_multfn
+c = np.ones(11)
+c[10] = 0
+mu, out = compute_optimal_multfn(c)
+assert np.allclose(mu, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+assert out == 0
+
+
+# Test symmetric_multfn
+q = np.array([1.0, 0.5, 0.0])
+mus, J = symmetric_multfn(q, subproblems=True)
+assert J[0] == 0  # optimal cost of partitioning no individual is 0
+assert J[1] == 1  # optimal cost of partitioning 1 individual is 1
+assert J[2] == 2  # optimal cost of testing two individuals
+assert np.all(np.diff(J) >= 0)  # nondecreasing
+assert np.allclose(np.array([[0, 0, 0], [0, 1, 0], [0, 2, 0]]), mus)
