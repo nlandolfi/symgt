dfc480bffadd897cb7756bca0972bceca9e93eab
Nicholas C. Landolfi <nicholas.charles@landolfi.org>
2023-05-18 22:03:03 -0700

    reorder

--- src/symgt/utils.py
+++ src/symgt/utils.py
@@ -19,6 +19,39 @@ def integer_partition_from_multfn(multfn: np.ndarray) -> np.ndarray:
     return np.asarray(ss, dtype=int)
 
 
+def ETests(q: np.ndarray, h: int) -> float:
+    """
+    Compute the expected number of tests used for a group of size `h`
+    for a symmetric distribution with representation `q`.
+
+    Parameters
+    ----------
+    q : np.ndarray
+        `q[i]` is the probability that a group of size i tests negative.
+    h : int
+        `h` is the size of the group.
+    """
+    if h == 1:
+        return 1
+    else:
+        return 1 + h * (1 - q[h])
+
+
+def ECost(q: np.ndarray, multfn: np.ndarray) -> float:
+    """
+    Compute the expected cost of a grouping encoded by `multfn` for a
+    symmetric distribution with representation `q`.
+
+    Parameters
+    ----------
+    multfn : np.ndarray
+        `multfn[i]` is the number of parts of size `i`.
+    q : np.ndarray
+        `q[i]` is the probability that a group of size i tests negative.
+    """
+    return np.sum([m * ETests(q, h) for (h, m) in enumerate(multfn)])
+
+
 def U_from_q(q: np.ndarray) -> np.ndarray:
     """
     Compute the function `U` for the symmetric distribution represented by `q`.
@@ -43,38 +76,5 @@ def U_from_q(q: np.ndarray) -> np.ndarray:
     U[0] = 1
     U[1] = 1
     for i in range(2, n + 1):
-        U[i] = 1 + i * (1 - q[i])
+        U[i] = 1 + i * (1 - q[i])  # don't bother calling ETests
     return U
-
-
-def ECost(q: np.ndarray, multfn: np.ndarray) -> float:
-    """
-    Compute the expected cost of a grouping encoded by `multfn` for a
-    symmetric distribution with representation `q`.
-
-    Parameters
-    ----------
-    multfn : np.ndarray
-        `multfn[i]` is the number of parts of size `i`.
-    q : np.ndarray
-        `q[i]` is the probability that a group of size i tests negative.
-    """
-    return np.sum([m * ETests(q, h) for (h, m) in enumerate(multfn)])
-
-
-def ETests(q: np.ndarray, h: int) -> float:
-    """
-    Compute the expected number of tests used for a group of size `h`
-    for a symmetric distribution with representation `q`.
-
-    Parameters
-    ----------
-    q : np.ndarray
-        `q[i]` is the probability that a group of size i tests negative.
-    h : int
-        `h` is the size of the group.
-    """
-    if h == 1:
-        return 1
-    else:
-        return 1 + h * (1 - q[h])
