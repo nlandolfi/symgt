a73b4c7687efaa17a4da92d3e652ad45b0ce997d
Nick Landolfi <nicholas.charles@landolfi.org>
2023-05-22 11:18:29 -0700

    utils: empirical_tests_used (#21)
    * draft of empirical_tests_used
    
    * cleanup, more tests
    
    * documentation cleanup
    
    * typo
    
    * switch to more clear casting
    
    * checkpoint
    
    * typo
    
    * checkpoint
    
    * checkpoint
    
    * checkpoint
    
    * checkpoint
    
    * checkpoint
    
    * checkpoint

--- src/symgt/utils.py
+++ src/symgt/utils.py
@@ -77,11 +77,11 @@ def U_from_q(q: np.ndarray) -> np.ndarray:
 
 def grouptest_array(multfn: np.ndarray) -> np.ndarray:
     """
-    Form a matrix that can be used to compute group tests statuses from individual
-    status vectors.
+    Form a matrix that can be used to compute the number of positives per
+    group (and, hence, group statuses) from individual status vectors.
 
     The matrix is `g` by `n` where `g = np.sum(multfn)` is the number of groups
-    and `n = np.dot(np.arange(len(multfn)), multfn)` is the population size. 
+    and `n = np.dot(np.arange(len(multfn)), multfn)` is the population size.
     The `i, j`th entry of the matrix is 1 if specimen `j` goes to group `i`.
 
     Large groups first. For example, if there is a group of size 1 and a group of
@@ -91,6 +91,7 @@ def grouptest_array(multfn: np.ndarray) -> np.ndarray:
     ```
         A = grouptest_array(multfn)
         positives_per_group = A @ x
+        group_statuses = (positives_per_group > 0).astype(int)
     ```
     Notice that `positives_per_group` may *not* be a binary vector. For example
     `A = np.array([[1, 1]])` and `x = np.array([1, 1])` will give `np.array([2])`.
@@ -115,3 +116,64 @@ def grouptest_array(multfn: np.ndarray) -> np.ndarray:
     for i in range(len(s)):
         A[i, cum_s[i] : cum_s[i + 1]] = 1
     return A
+
+
+def empirical_tests_used(A: np.ndarray, X: np.ndarray) -> int:
+    """
+    Compute the number of tests used where `A` is a 2D array of group
+    test assignments (see `grouptest_array`) and `X` is a 2D array
+    whose rows are individual status vectors (the data matrix).
+
+    Examples
+    --------
+    With `multfn` and data matrix `X`,
+    ```
+        A = grouptest_array(multfn)
+        empirical_tests_used(A, X)
+    ```
+
+    Parameters
+    ----------
+    A : np.ndarray
+        `A[i, j]` is 1 if specimen `j` is included in pool `i`.
+    X : np.ndarray
+        `X[i, :]` is the `i`th sample of individual status vectors.
+
+    Returns
+    -------
+    int
+        The number of tests used.
+    """
+    if not (np.all(np.isin(A, [0, 1]))):
+        raise ValueError("group test array should be a binary matrix")
+    if not (np.all(np.isin(X, [0, 1]))):
+        raise ValueError("sample array should be a binary matrix")
+
+    g, n = A.shape
+    if not np.all(np.ones(n) == np.sum(A, axis=0)):
+        raise ValueError("unexpected: group test array has overlapping pools")
+
+    N, n2 = X.shape
+    if n != n2:
+        raise ValueError(
+            f"differing population size: group test array has {n} \
+                    and sample matrix has {n2}"
+        )
+
+    # a g-vector
+    group_sizes_vector = np.sum(A, axis=1)
+    if np.sum(group_sizes_vector) != n:
+        raise ValueError(
+            f"group sizes should sum to {n}, \
+                got {np.sum(group_sizes_vector)}"
+        )
+
+    # an N x g matrix
+    group_statuses_matrix = (X @ A.T > 0).astype(int)
+
+    # first term is group tests, second is individual retests
+    used = (g * N) + np.sum(group_statuses_matrix @ group_sizes_vector)
+
+    assert used == int(used), f"result should be an integer, but got {r}"
+
+    return int(used)
